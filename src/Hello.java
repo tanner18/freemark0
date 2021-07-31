d954e4187aad5c1cf6eacab39116c4b105a503af
wt <windsor@wtanner.de>
2021-07-31 17:15:32 +0200

    Added template folder, template (freemarker)

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,72 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateExceptionHandler;
+import freemarker.template.Version;
+
 public class Hello {
 
+    public static void main(String[] args) throws Exception {
 
+        // 1. Configure FreeMarker
+        //
+        // You should do this ONLY ONCE, when your application starts,
+        // then reuse the same Configuration object elsewhere.
+        Configuration cfg = new Configuration();
+
+        // from where is template loaded?
+        cfg.setDirectoryForTemplateLoading(new File("/home/hawking/workspace/java/Free0/templates"));
+        //cfg.setClassForTemplateLoading(Hello.class,"templates");
+
+        //other recommended settings:
+        cfg.setIncompatibleImprovements(new Version(2,3,20));
+        cfg.setDefaultEncoding("UTF-8");
+        cfg.setLocale(Locale.US);
+
+        // You will do this for several times in typical applications.
+
+        // 2.1. Prepare the template input:
+
+        Map<String, Object> input = new HashMap<String, Object>();
+
+        input.put("title", "Vogella example");
+
+        input.put("exampleObject", new ValueExampleObject("Java object", "me"));
+
+        List<ValueExampleObject> systems = new ArrayList<ValueExampleObject>();
+        systems.add(new ValueExampleObject("Android", "Google"));
+        systems.add(new ValueExampleObject("iOS States", "Apple"));
+        systems.add(new ValueExampleObject("Ubuntu", "Canonical"));
+        systems.add(new ValueExampleObject("Windows7", "Microsoft"));
+        input.put("systems", systems);
+
+        // 2.2. Get the template
+
+        Template template = cfg.getTemplate("hello.ftl");
+
+        // 2.3. Generate the output
+
+        // Write output to the console
+        Writer consoleWriter = new OutputStreamWriter(System.out);
+        template.process(input, consoleWriter);
+
+        // For the sake of example, also write output into a file:
+        Writer fileWriter = new FileWriter(new File("output.html"));
+        try {
+            template.process(input, fileWriter);
+        } finally {
+            fileWriter.close();
+        }
+
+    }
 
 }
---
+++ src/ValueExampleObject.java
@@ -0,0 +1,20 @@
+public class ValueExampleObject {
+
+    private String name;
+    private String developer;
+
+    // constructor
+    public ValueExampleObject(String name, String developer) {
+        this.name = name;
+        this.developer = developer;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDeveloper() {
+        return developer;
+    }
+
+}
